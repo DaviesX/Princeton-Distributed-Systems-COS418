6856d3fc58c1f9161adaaadf113707d4baede367
Chifeng Wen <chifenw@google.com>
2021-05-14 03:23:32 -0700

    Implemented the doMap function.
    This implements the map phase's major procedure to map file content to a
    list of key-value pairs, then shards the pairs into multiple
    mini-reducer files by key.

--- cos418/mapreduce/common_map.go
+++ cos418/mapreduce/common_map.go
@@ -1,7 +1,11 @@
 package mapreduce
 
 import (
+	"encoding/json"
+	"fmt"
 	"hash/fnv"
+	"io/ioutil"
+	"os"
 )
 
 // doMap does the job of a map worker: it reads one of the input files
@@ -40,6 +44,52 @@ func doMap(
 	//     err := enc.Encode(&kv)
 	//
 	// Remember to close the file after you have written all the values!
+	fileContent, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		fmt.Printf(
+			"Failed to read file=%s, err=%s\n",
+			inFile,
+			err.Error())
+		panic(err)
+	}
+
+	// Map file content to key-value pairs.
+	kvs := mapF(inFile, string(fileContent))
+
+	// Creates per-reducer JSON serializer.
+	encoders := make([]*json.Encoder, nReduce)
+	for i := 0; i < nReduce; i++ {
+		outputFileName := reduceName(jobName, mapTaskNumber, i)
+
+		reducerFile, err := os.OpenFile(
+			outputFileName, os.O_CREATE|os.O_WRONLY, 0777)
+		if err != nil {
+			fmt.Printf(
+				"Faild to open file=%s in write mode, err=%s\n",
+				outputFileName, err.Error())
+			panic(err)
+		}
+
+		defer reducerFile.Close()
+
+		encoders[i] = json.NewEncoder(reducerFile)
+	}
+
+	// Put each key-value pair to its corresponding reducer file
+	// sharded by key.
+	for _, kv := range kvs {
+		reducerTaskNumber := ihash(kv.Key) % uint32(nReduce)
+		err := encoders[reducerTaskNumber].Encode(&kv)
+
+		if err != nil {
+			fmt.Printf(
+				"Failed to encode (k=%s, v=%s), err=%s\n",
+				kv.Key,
+				kv.Value,
+				err.Error())
+			panic(err)
+		}
+	}
 }
 
 func ihash(s string) uint32 {
