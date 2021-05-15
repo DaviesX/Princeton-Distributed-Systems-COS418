2b0df4a1299c9e7fd4052ed785eec0067485aae2
Chifeng Wen <chifenw@google.com>
2021-05-14 20:36:26 -0700

    Implements the doReduce function.
    It covers the major procedure during the reduce phase:
    1. It loads the reducer files, produced from map tasks, corresponding to the
    reduceTaskNumber
    2. It performs a group by key operation and pass the grouped values to
    the reducer function
    3. Stream out the result computed from the reducer function to the
    result split which will be merged later by the master.

--- cos418/mapreduce/common_reduce.go
+++ cos418/mapreduce/common_reduce.go
@@ -1,5 +1,40 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func LoadKvs(file *os.File, kvs *[]KeyValue) {
+	decoder := json.NewDecoder(file)
+
+	var kv KeyValue
+	decoder_err := decoder.Decode(&kv)
+	for decoder_err == nil {
+		*kvs = append(*kvs, kv)
+		decoder_err = decoder.Decode(&kv)
+	}
+}
+
+func NextKey(
+	kvs []KeyValue, i *int) (key string, values []string, err error) {
+	if *i >= len(kvs) {
+		return "", make([]string, 0), errors.New("end of slice")
+	}
+
+	theKey := kvs[*i].Key
+	valuesGroupByKey := make([]string, 0)
+
+	for ; *i < len(kvs) && kvs[*i].Key == theKey; *i++ {
+		valuesGroupByKey = append(valuesGroupByKey, kvs[*i].Value)
+	}
+
+	return theKey, valuesGroupByKey, nil
+}
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -10,25 +45,54 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the intermediate file for this reduce task from map task number
-	// m using reduceName(jobName, m, reduceTaskNumber).
-	// Remember that you've encoded the values in the intermediate files, so you
-	// will need to decode them. If you chose to use JSON, you can read out
-	// multiple decoded values by creating a decoder, and then repeatedly calling
-	// .Decode() on it until Decode() returns an error.
-	//
-	// You should write the reduced output in as JSON encoded KeyValue
-	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
-	// you to use JSON here because that is what the merger than combines the
-	// output from all the reduce tasks expects. There is nothing "special" about
-	// JSON -- it is just the marshalling format we chose to use. It will look
-	// something like this:
-	//
-	// enc := json.NewEncoder(mergeFile)
-	// for key in ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
+	// Collects all the imtermediate data produced from all
+	// the mapper tasks.
+	kvs := make([]KeyValue, 0)
+	for i := 0; i < nMap; i++ {
+		inputFileName := reduceName(jobName, i, reduceTaskNumber)
+		inputFile, err := os.Open(inputFileName)
+		if err != nil {
+			fmt.Printf(
+				"Failed to open reducer file=%s, err=%s",
+				inputFileName,
+				err.Error())
+			panic(err)
+		}
+
+		LoadKvs(inputFile, &kvs)
+		inputFile.Close()
+	}
+
+	sort.Slice(
+		kvs,
+		func(i, j int) bool {
+			return kvs[i].Key < kvs[j].Key
+		})
+
+	// Prepares the output to store the result split for the
+	// current reducer task.
+	outputFileName := mergeName(jobName, reduceTaskNumber)
+	outputFile, err := os.OpenFile(
+		outputFileName, os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		fmt.Printf(
+			"Failed to open split file=%s, err=%s",
+			outputFileName,
+			err.Error())
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	outputEncoder := json.NewEncoder(outputFile)
+
+	// Reduce the key-value pairs key by key and commit the
+	// reduction to the result split.
+	var iterator int = 0
+	key, values, err := NextKey(kvs, &iterator)
+	for err == nil {
+		reductionResult := reduceF(key, values)
+		outputEncoder.Encode(KeyValue{key, reductionResult})
+
+		key, values, err = NextKey(kvs, &iterator)
+	}
 }
